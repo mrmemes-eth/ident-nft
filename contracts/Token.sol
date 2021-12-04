37520b860ddbabb81a36bf59f1c053a1abc7bd2d
Stephen Caudill <scaudill@gmail.com>
2021-12-04 15:39:00 -0700

    Quiet down contract warnings

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,7 @@
+//SPDX-License-Identifier: UNLICENSED
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.7.0;
+pragma solidity >=0.7.0;
 
 import "hardhat/console.sol";
 
