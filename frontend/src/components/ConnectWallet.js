baabc9834ce2fb17f594b6ee13f60f349dc99307
Jarek S <jarek.sygitowicz@authologic.com>
2021-12-05 10:48:35 +0100

    smol fixes

--- frontend/src/components/ConnectWallet.js
+++ frontend/src/components/ConnectWallet.js
@@ -16,13 +16,15 @@ export function ConnectWallet({ connectWallet, networkError, dismiss }) {
           )}
         </div>
         <div className="col-6 p-4 text-center">
-          <p>Please connect to your wallet.</p>
+          <p class="text-xl">This is a proof of concept</p>
           <button
-            className="bg-white shadow-sm hover:shadow-xl font-semibold text-gray-500 rounded-3xl border-2 border-gray-300 p-3 flex"
+            className="mx-auto bg-white shadow-sm hover:shadow-xl font-semibold text-gray-500 rounded-3xl border-2 border-gray-300 p-3 flex"
             type="button"
             onClick={connectWallet}
           >
-            <div class="rounded-md bg-gradient-to-br from-blue-900 to-pink-700 w-6 h-6 ml-6"></div> 
+            <div class="animate-ping absolute rounded-md bg-gradient-to-br from-blue-900 to-pink-700 w-6 h-6 ml-6"></div> 
+            <div class="relative rounded-md bg-gradient-to-br from-blue-900 to-pink-700 w-6 h-6 ml-6"></div> 
+
             <div class="pl-4 mr-6">Sign in with NFT</div>
           </button>
         </div>
