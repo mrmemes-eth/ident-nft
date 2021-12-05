2517095c08f5609381b8d9a96d04c631f46ddfa9
Stephen Caudill <scaudill@gmail.com>
2021-12-05 15:23:02 -0700

    WIP: owner problems

--- contracts/NFTPass.sol
+++ contracts/NFTPass.sol
@@ -3,29 +3,65 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTPass is Ownable, ERC721Enumerable {
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+contract NFTPass is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
 
-  constructor() ERC721 ("NFTPass", "Pass") {
-    console.log("NFTPass contract.");
-  }
+    Counters.Counter private _tokenIdCounter;
 
-  function mintNFTPass() public returns (uint256) {
-    _tokenIds.increment();
-    uint256 newItemId = _tokenIds.current();
-    _safeMint(msg.sender, newItemId);
+    constructor() ERC721("NFTPass", "MTK") {
+        console.log("NFTPass contract.");
+    }
 
-    console.log("Minting NFTPass[%s] to Recipient: %s", newItemId, msg.sender);
-    return newItemId;
-  }
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        console.log("Minting NFTPass[%s] to: %s", tokenId, to);
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
 
-  function send(address recipient, uint256 tokenId) public {
-    safeTransferFrom(ownerOf(tokenId), recipient, tokenId);
-  }
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function mintNFTPass() public {
+      safeMint(msg.sender, "https://jsonkeeper.com/b/WD5R");
+    }
+
+    function send(address recipient, uint256 tokenId) public {
+      safeTransferFrom(ownerOf(tokenId), recipient, tokenId);
+    }
 }
