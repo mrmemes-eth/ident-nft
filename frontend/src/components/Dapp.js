b2309d52626d5a7e6b5b74560f5084750054508f
Jarek S <jarek.sygitowicz@authologic.com>
2021-12-05 18:07:12 +0100

    header improvements

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -105,11 +105,11 @@ export class Dapp extends React.Component {
         <header class="w-full absolute top-0 h-14">
           <div class="flex top-0 items-center justify-between pr-4 pl-4 h-16">
             <div class="flex font-bold text-gray-600">NFT Authentication Framework MVP</div>
-            <div class="p-2 border-2 border-gray-200 rounded-xl text-xs text-right text-gray-500 flex items-center">
+            <div class="p-2 border-1 shadow-md border-gray-200 rounded-xl text-right text-gray-500 bg-white flex items-center">
               <div class="rounded-3xl w-8 h-8 bg-gradient-to-br from-blue-900 to-pink-700 mr-2"></div>
               <div class="flex flex-col">
-                <div class="w-full text-left">{this.state.selectedAddress}</div>
-                <div class="w-full text-left">You have: {this.state.balance.toString()} {this.state.tokenData.symbol}
+                <div class="w-full text-md text-left">{this.state.selectedAddress.substring(0, 5)}(...)</div>
+                <div class="w-full text-xs text-left">You have: {this.state.balance.toString()} {this.state.tokenData.symbol}
                 </div>
               </div>
             </div>
